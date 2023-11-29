37581e918db79e5bdfcae5a2460ed87eb8b5c366
OksanaMosk <130758466+OksanaMosk@users.noreply.github.com>
2023-11-29 16:13:41 +0200

    1

--- src/redux/contacts/contacts.reducer.js
+++ src/redux/contacts/contacts.reducer.js
@@ -5,12 +5,10 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   contacts: [
-    JSON.parse(localStorage.getItem('contacts')) ?? [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-123-5633' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-893-1233' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-173-7933' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-913-2633' },
-    ],
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-123-5633' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-893-1233' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-173-7933' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-913-2633' },
   ],
 };
 
